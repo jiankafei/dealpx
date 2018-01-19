e64069533bc5bdf0455882530b56a1481de4a4ef
zr <Wll001992Yx@163.com>
2018-01-19 22:06:23 +0800

    fixed

--- src/trans.js
+++ src/trans.js
@@ -7,7 +7,7 @@ const coverter = require('./index');
 const css = fs.readFileSync(path.resolve(__dirname, 'index.css'), 'utf8');
 const processCSS = postcss(coverter()).process(css).css;
 
-fs.writeFile(path.resolve(__dirname, '..', '/static/index.css'), processCSS, err => {
+fs.writeFile(path.resolve(__dirname, '..', 'static/index.css'), processCSS, err => {
 	if (err) throw err;
 	console.log('Translate unit success.');
 });
